bf29266ac0e517b2ee525104cf87878eb1a37d1c
Younesid12 <idyounes95@gmail.com>
2023-11-15 16:23:35 -0800

    updated

--- _cd.c
+++ _cd.c
@@ -25,19 +25,20 @@ int _cd(char **argv, char *buffer, char *path)
 		if (argv[1] == NULL || strncmp("-", argv[1], 1) == 0)
 		{
 			home = getenv("HOME");
-			if (home != NULL)
-				chdir(home);
+			if (home != NULL && chdir(home) != 0)
+			{
+				perror("chdir");
+				return (EXIT_FAILURE);
+			}
 		}
 		if (getcwd(current_dir, sizeof(current_dir)) != NULL)
 		{
 			setenv("PWD", current_dir, 1);
-			return (0);
+			return (EXIT_SUCCESS);
 		}
 		else
 		{
 			perror("getcwd");
-			return (-1);
+			return (EXIT_FAILURE);
 		}
-		free_token(argv, buffer, NULL);
-		return (0);
 }
